e89abfda63f2f7a162396250eb26686e69ee14a0
Cathy <45395549+CathyDolle@users.noreply.github.com>
2026-04-20 11:44:35 +0200

    good imgs

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,23 +1,161 @@
-import i1 from "./i1.jpg";
-import i2 from "./i2.jpg";
-import i3 from "./i3.jpg";
-import i4 from "./i4.webp";
-import i5 from "./i5.webp";
-import i6 from "./i6.jpg";
-import i7 from "./i7.jpg";
-import i8 from "./i8.jpg";
-import i9 from "./i9.jpg";
+import img001 from "./chanel/001.jpg";
+import img002 from "./chanel/002.jpg";
+import img003 from "./chanel/003.jpg";
+import img004 from "./chanel/004.jpg";
+import img005 from "./chanel/005.jpg";
+import img006 from "./chanel/006.jpg";
+import img007 from "./chanel/007.jpg";
+import img008 from "./chanel/008.jpg";
+import img009 from "./chanel/009.jpg";
+import img010 from "./chanel/010.jpg";
+import img011 from "./chanel/011.jpg";
+import img012 from "./chanel/012.jpg";
+import img013 from "./chanel/013.jpg";
+import img014 from "./chanel/014.jpg";
+import img015 from "./chanel/015.jpg";
+import img016 from "./chanel/016.jpg";
+import img017 from "./chanel/017.jpg";
+import img018 from "./chanel/018.jpg";
+import img019 from "./chanel/019.jpg";
+import img020 from "./chanel/020.jpg";
+import img021 from "./chanel/021.jpg";
+import img022 from "./chanel/022.jpg";
+import img023 from "./chanel/023.jpg";
+import img024 from "./chanel/024.jpg";
+import img025 from "./chanel/025.jpg";
+import img026 from "./chanel/026.jpg";
+import img027 from "./chanel/027.jpg";
+import img028 from "./chanel/028.jpg";
+import img029 from "./chanel/029.jpg";
+import img030 from "./chanel/030.jpg";
+import img031 from "./chanel/031.jpg";
+import img032 from "./chanel/032.jpg";
+import img033 from "./chanel/033.jpg";
+import img034 from "./chanel/034.jpg";
+import img035 from "./chanel/035.jpg";
+import img036 from "./chanel/036.jpg";
+import img037 from "./chanel/037.jpg";
+import img038 from "./chanel/038.jpg";
+import img039 from "./chanel/039.jpg";
+import img040 from "./chanel/040.jpg";
+import img041 from "./chanel/041.jpg";
+import img042 from "./chanel/042.jpg";
+import img043 from "./chanel/043.jpg";
+import img044 from "./chanel/044.jpg";
+import img045 from "./chanel/045.jpg";
+import img046 from "./chanel/046.jpg";
+import img047 from "./chanel/047.jpg";
+import img048 from "./chanel/048.jpg";
+import img049 from "./chanel/049.jpg";
+import img050 from "./chanel/050.jpg";
+import img051 from "./chanel/051.jpg";
+import img052 from "./chanel/052.jpg";
+import img053 from "./chanel/053.jpg";
+import img054 from "./chanel/054.jpg";
+import img055 from "./chanel/055.jpg";
+import img056 from "./chanel/056.jpg";
+import img057 from "./chanel/057.jpg";
+import img058 from "./chanel/058.jpg";
+import img059 from "./chanel/059.jpg";
+import img060 from "./chanel/060.jpg";
+import img061 from "./chanel/061.jpg";
+import img062 from "./chanel/062.jpg";
+import img063 from "./chanel/063.jpg";
+import img064 from "./chanel/064.jpg";
+import img065 from "./chanel/065.jpg";
+import img066 from "./chanel/066.jpg";
+import img067 from "./chanel/067.jpg";
+import img068 from "./chanel/068.jpg";
+import img069 from "./chanel/069.jpg";
+import img070 from "./chanel/070.jpg";
+import img071 from "./chanel/071.jpg";
+import img072 from "./chanel/072.jpg";
+import img073 from "./chanel/073.jpg";
+import img074 from "./chanel/074.jpg";
+import img075 from "./chanel/075.jpg";
+import img076 from "./chanel/076.jpg";
+import img077 from "./chanel/077.jpg";
+import img078 from "./chanel/078.jpg";
 
 export const lookbookImages = {
-  i1,
-  i2,
-  i3,
-  i4,
-  i5,
-  i6,
-  i7,
-  i8,
-  i9,
+  "001": img001,
+  "002": img002,
+  "003": img003,
+  "004": img004,
+  "005": img005,
+  "006": img006,
+  "007": img007,
+  "008": img008,
+  "009": img009,
+  "010": img010,
+  "011": img011,
+  "012": img012,
+  "013": img013,
+  "014": img014,
+  "015": img015,
+  "016": img016,
+  "017": img017,
+  "018": img018,
+  "019": img019,
+  "020": img020,
+  "021": img021,
+  "022": img022,
+  "023": img023,
+  "024": img024,
+  "025": img025,
+  "026": img026,
+  "027": img027,
+  "028": img028,
+  "029": img029,
+  "030": img030,
+  "031": img031,
+  "032": img032,
+  "033": img033,
+  "034": img034,
+  "035": img035,
+  "036": img036,
+  "037": img037,
+  "038": img038,
+  "039": img039,
+  "040": img040,
+  "041": img041,
+  "042": img042,
+  "043": img043,
+  "044": img044,
+  "045": img045,
+  "046": img046,
+  "047": img047,
+  "048": img048,
+  "049": img049,
+  "050": img050,
+  "051": img051,
+  "052": img052,
+  "053": img053,
+  "054": img054,
+  "055": img055,
+  "056": img056,
+  "057": img057,
+  "058": img058,
+  "059": img059,
+  "060": img060,
+  "061": img061,
+  "062": img062,
+  "063": img063,
+  "064": img064,
+  "065": img065,
+  "066": img066,
+  "067": img067,
+  "068": img068,
+  "069": img069,
+  "070": img070,
+  "071": img071,
+  "072": img072,
+  "073": img073,
+  "074": img074,
+  "075": img075,
+  "076": img076,
+  "077": img077,
+  "078": img078,
 } as const;
 
 export type LookbookImageKey = keyof typeof lookbookImages;
